dabb088b915ff06628548587aa8369353772c0a0
Matt Gray <matt@mattgrayisok.com>
2020-09-11 14:01:51 +0100

    Only load CSRF tokens on pages which will actually use them

--- src/Plugin.php
+++ src/Plugin.php
@@ -65,25 +65,30 @@ class Plugin extends \craft\base\Plugin
     {
         $view = Craft::$app->getView();
 
+        $csrfTokenName = Craft::$app->getConfig()->getGeneral()->csrfTokenName;
+
         if (!Craft::$app->getRequest()->getIsCpRequest()) {
             $url = '/' . Craft::$app->getConfig()->getGeneral()->actionTrigger . '/servd-asset-storage/csrf-token/get-token';
             $view->registerJs('
+                window.SERVD_CSRF_TOKEN_NAME = "' . $csrfTokenName . '";
                 function injectCSRF() {
-                    var xhr = new XMLHttpRequest();
-                    xhr.onload = function () {
-                        if (xhr.status >= 200 && xhr.status <= 299) {
-                            var tokenInfo = JSON.parse(this.responseText);
-                            window.csrfTokenName = tokenInfo.name;
-                            window.csrfTokenValue = tokenInfo.token;
-                            var inputs = document.getElementsByName(tokenInfo.name);
-                            var len = inputs.length;
-                            for (var i=0; i<len; i++) {
-                                inputs[i].setAttribute("value", tokenInfo.token);
+                    var inputs = document.getElementsByName(window.SERVD_CSRF_TOKEN_NAME);
+                    var len = inputs.length;
+                    if (len > 0) {
+                        var xhr = new XMLHttpRequest();
+                        xhr.onload = function () {
+                            if (xhr.status >= 200 && xhr.status <= 299) {
+                                var tokenInfo = JSON.parse(this.responseText);
+                                window.csrfTokenValue = tokenInfo.token;
+                                window.csrfTokenName = tokenInfo.name;
+                                for (var i=0; i<len; i++) {
+                                    inputs[i].setAttribute("value", tokenInfo.token);
+                                }
                             }
-                        }
-                    };
-                    xhr.open("GET", "' . $url . '");
-                    xhr.send();
+                        };
+                        xhr.open("GET", "' . $url . '");
+                        xhr.send();
+                    }
                 }
                 setTimeout(injectCSRF, 200);
             ', View::POS_END);
