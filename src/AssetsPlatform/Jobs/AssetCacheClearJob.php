df6c5fab1119941c803dca253345a877f3cda6bb
Matt Gray <matt@mattgrayisok.com>
2020-12-13 17:51:09 +0000

    Asset cache invalidation

---
+++ src/AssetsPlatform/Jobs/AssetCacheClearJob.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace servd\AssetStorage\AssetsPlatform\Jobs;
+
+use Craft;
+use craft\queue\BaseJob;
+use Exception;
+use GuzzleHttp\Client;
+use servd\AssetStorage\Plugin;
+
+class AssetCacheClearJob extends BaseJob
+{
+    public $elementUid;
+    private $cacheClearUrl = 'https://app.servd.host/asset-platform-clear-cache';
+
+    public function execute($queue)
+    {
+        $elements = Craft::$app->elements;
+        $element = $elements->getElementByUid($this->elementUid);
+
+        $settings = Plugin::$plugin->getSettings();
+        $slug = $settings->getProjectSlug();
+        $environment = $settings->getAssetsEnvironment();
+        $assetPath = $element->path;
+        $securityKey = $settings->getSecurityKey();
+
+        $assetPath = '/' . $slug . '/' . $environment . '/' . $assetPath;
+
+        $data = [
+            'slug' => $slug,
+            'assetPath' => $assetPath,
+            'key' => $securityKey
+        ];
+
+        $url = $this->cacheClearUrl;
+        if (!empty(getenv('SERVD_ASSET_CACHE_CLEAR_URL'))) {
+            $url = getenv('SERVD_ASSET_CACHE_CLEAR_URL');
+        }
+
+        try {
+            $client = new Client();
+            $response = $client->post($url, $data);
+        } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+            throw new Exception("Failed to contact Servd cache clear endpoint: " . $e->getMessage());
+        }
+    }
+
+    protected function defaultDescription()
+    {
+        return 'Clear Servd CDN Cache';
+    }
+}
