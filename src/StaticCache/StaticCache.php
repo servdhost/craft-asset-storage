50f660edea6af6551e2aa52ca8540013afd3955a
Matt Gray <matt@mattgrayisok.com>
2021-02-09 23:44:41 +0000

    I hate brackets

--- src/StaticCache/StaticCache.php
+++ src/StaticCache/StaticCache.php
@@ -103,7 +103,7 @@ class StaticCache extends Component
         if (
             $request->getIsCpRequest() || $request->getIsLivePreview() ||
             $request->getIsActionRequest() || !$request->getIsGet() ||
-            $request->headers['X-Servd-Cache'] ?? '0' !== '1'
+            ($request->headers['x-servd-cache'] ?? '0') !== '1'
         ) {
             return false;
         }
